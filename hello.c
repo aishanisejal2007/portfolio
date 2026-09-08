2640c2e205432eee8333d18ee21e065c917a8b01
Aishani sejal <ugcet2501976@reva.edu.in>
2026-09-08 11:39:43 +0530

    Add greet() function - paired with YOUR_PARTNER_NAME via Live Share

--- hello.c
+++ hello.c
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
 #include<stdio.h>
 int main(){
     printf("Hello world!\n");
     return 0;
-=======
-#include<stdio.h>
-int main(){
-    printf("Hello world!\n");
-    return 0;
->>>>>>> f9cd70b85b7da90385b2a33ed3004c9394fc9ec6
 }
